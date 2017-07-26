91889f21df7b58a44d32b0464a8ad26ec51bf8e6
Com422Mac10 <com422mac10@Com422Mac10s-iMac.local>
2017-07-26 09:43:38 +0700

    Learn Array

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//การใช้ ตัวแปร Array
+//Implicit and Assign Vakue
+let intArrayNumber1 = [11,22,33,44,55]
+
+// Exelicit And Assign Vlue
+
+var intArrayNumber2 = [Int]()
+intArrayNumber2 = [66,77,88,99]
+
+// String Type
+let strName1 = ["Dotamon","Nopita"]
+var strName2 = [String]()
+strName2=["Somchai","Pichan"]
+
+//Double Type
+let douNumber1 = [1.2,1.3,1.4,]
+var douNumber2 = [Double] ()
+douNumber2 = [2.1,2.2]
+
