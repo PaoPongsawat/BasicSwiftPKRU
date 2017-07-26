6e25e0afe54e3263f141e95c7fc2219b44766efb
Com422Mac10 <com422mac10@Com422Mac10s-iMac.local>
2017-07-26 09:59:07 +0700

    AddArray

--- Array.playground/Contents.swift
+++ Array.playground/Contents.swift
@@ -4,7 +4,7 @@ import UIKit
 
 //การใช้ ตัวแปร Array
 //Implicit and Assign Vakue
-let intArrayNumber1 = [11,22,33,44,55]
+var intArrayNumber1 = [11,22,33,44,55]
 
 // Exelicit And Assign Vlue
 
@@ -12,7 +12,7 @@ var intArrayNumber2 = [Int]()
 intArrayNumber2 = [66,77,88,99]
 
 // String Type
-let strName1 = ["Dotamon","Nopita"]
+var strName1 = ["Dotamon","Nopita"]
 var strName2 = [String]()
 strName2=["Somchai","Pichan"]
 
@@ -21,3 +21,10 @@ let douNumber1 = [1.2,1.3,1.4,]
 var douNumber2 = [Double] ()
 douNumber2 = [2.1,2.2]
 
+//การเพิ่มสมาชิกให้กับตัวแปร Array
+
+print("intArrayNamber1 ==> \(intArrayNumber1)")
+intArrayNumber1.append(999)
+print("นี่คือ intArrayNumber ใหม่ ==>\(intArrayNumber1)")
+print("strName1 ==> \(strName1)")
+strName1.append("MasterUNg")
