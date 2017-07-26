ee1f6f4214c871425bd1cdb3e152c3f119008503
Com422Mac10 <com422mac10@Com422Mac10s-iMac.local>
2017-07-26 11:43:47 +0700

    Add Member

--- add Array/add Array/ViewController.swift
+++ add Array/add Array/ViewController.swift
@@ -10,22 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    var strArrayMember = ["Master"]
     
     @IBOutlet weak var memberTextField: UITextField!
     
-    
-    
-    
     @IBOutlet weak var memberLabel: UILabel!
-    
-    
-    
     
     @IBAction func addButton(_ sender: Any) {
         
+        //Get Value From TextField
+        let strMember = memberTextField.text
+        print("strMember ==>\(String(describing: strMember))")
         
-    }//addButton
+        print("strArrayMember before ==> \(strArrayMember)")
+        strArrayMember.append(strMember!)
+        print("strArrayMember after ==> \(strArrayMember)")
+        
+        //Show Member
+        
+        memberLabel.text = strArrayMember[0]
+        
+        
+    }   //addButton
     
     
     
@@ -34,6 +40,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        memberLabel.sizeToFit()
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
