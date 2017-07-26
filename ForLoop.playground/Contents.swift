fad0d455a3a7a6ff252e8744f28482fda8ebe440
Com422Mac10 <com422mac10@Com422Mac10s-iMac.local>
2017-07-26 12:59:38 +0700

    new

---
+++ ForLoop.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Implicit
+let intNumber = [11,22,33,44,55,66,77]
+var intStart = 0
+let intEnd = intNumber.count
+
+for intArray in intNumber {
+    
+    print("intNumber[\(intStart)] ==> \(intNumber[intStart])")
+    intStart += 1
+    
+    
+} // for
+
+    //Implicit for String
+
+let strName = ["Doramon","Nopita","Sunako"]
+var intIndex = 0
+
+for strShowName in strName {
+    print ("strShowName ครั้งที่ \(intIndex) มีค่าเท่ากับ \(strName[intIndex])")
+    intIndex += 1
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
