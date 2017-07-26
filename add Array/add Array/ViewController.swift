05b5c1ad2711de539f01395a475de379f66587b6
Com422Mac10 <com422mac10@Com422Mac10s-iMac.local>
2017-07-26 11:13:04 +0700

    D

---
+++ add Array/add Array/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  add Array
+//
+//  Created by Com422Mac10 on 7/26/2560 BE.
+//  Copyright © 2560 Com422Mac10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var memberTextField: UITextField!
+    
+    
+    
+    
+    @IBOutlet weak var memberLabel: UILabel!
+    
+    
+    
+    
+    @IBAction func addButton(_ sender: Any) {
+        
+        
+    }//addButton
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
